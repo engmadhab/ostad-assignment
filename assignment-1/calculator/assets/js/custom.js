b631c0f26dd805ed34aaf6efe2cd82e5184fbbe2
Madhab <madhabkumarjoy@gmail.com>
2024-04-14 17:15:26 +0600

    update validation

--- assignment-1/calculator/assets/js/custom.js
+++ assignment-1/calculator/assets/js/custom.js
@@ -1,3 +1,46 @@
+const calcForm = document.getElementById('calculatorForm');
+let firstNumber = document.getElementById('firstInput');
+let secondNumber = document.getElementById('secondInput');
+let operator = document.getElementById('operator');
+
+// Show Error Method
+function showError(input,message){
+  const formControl = input.parentElement;
+  formControl.className='form-control error';
+
+  const small= formControl.querySelector('small');
+  small.innerText = message;
+} 
+
+// Form Validation Event Listeners
+
+calcForm.addEventListener('submit',function(e){
+  e.preventDefault();
+
+  if(firstNumber.value==''){
+    showError(firstNumber,'First number is required');
+  }else if(firstNumber.value!=firstNumber.value.replace(/[^0-9]/g)){
+    showError(firstNumber,'Only number is required');
+  }else{
+    showSuccess(firstNumber);
+  }
+
+  if(operator.value==''){
+    showError(operator,'Operator is required');
+  }else{
+    showSuccess(operator);
+  }
+
+  if(secondNumber.value==''){
+    showError(secondNumber,'Second number is required');
+  }else if(secondNumber.value!=secondNumber.value.replace(/[^0-9]/g)){
+    showError(secondNumber,'Only number is required');
+  }else{
+    showSuccess(secondNumber);
+  }
+
+});
+
 function calculate(){
   let firstNumber = parseFloat(document.getElementById('firstInput').value);
   let secondNumber = parseFloat(document.getElementById('secondInput').value);
@@ -15,5 +58,5 @@ function calculate(){
     result ="Invalid input";
   }
 
-  document.getElementById('getresult').innerHTML='Result is ' + result ;
+  document.getElementById('getresult').innerHTML='Result is = ' + result ;
 }
